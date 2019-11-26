fbb1290ed6da15a6d7154359d72965cb5e013d0f
Burke Ritter <burkeritter@outlook.com>
2019-11-26 10:16:37 -0500

    fixed ip

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ var app = express();
   
 // Creates a server which runs on port 3000 and  
 // can be accessed through localhost:3000 
-app.listen(3000, function() { 
-    console.log('server running on port 3000'); 
-} ) 
+app.listen(3000, "192.168.0.2") 
   
 // Function callName() is executed whenever  
 // url is of the form localhost:3000/stop 
